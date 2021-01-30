b1a826f73abe6fb0febaff6cb1eac03f1ef0a854
AidenDan <1299930560@qq.com>
2021-01-30 12:22:14 +0800

    整合阿里云Oss上传;添加用户头像上传模块

---
+++ service/service-oss/src/main/java/com/aiden/oss/OSSMain.java
@@ -0,0 +1,21 @@
+package com.aiden.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Aiden
+ * @version 1.0
+ * @description
+ * @date 2021-1-30 11:53:02
+ */
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // 排除数据源的自动配置，否则报错
+@ComponentScan(basePackages = {"com.aiden"}) // 扫描所有模块、子模块的com.aiden包下的类
+public class OSSMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OSSMain.class, args);
+    }
+}
