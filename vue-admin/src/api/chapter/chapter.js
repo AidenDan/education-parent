26f53dd7aa1318c324466a1fc6fcd81a8b87cf4f
AidenDan <1299930560@qq.com>
2021-02-06 20:25:35 +0800

    [bugfix]前端页面创建课程大纲组件ui居中调整

--- vue-admin/src/api/chapter/chapter.js
+++ vue-admin/src/api/chapter/chapter.js
@@ -4,7 +4,7 @@ export default {
   // 根据课程id获取课程信息
   getChapterInfoByCourseId(courseId) {
     return request({
-      url: `/education/edu-course/getChapterInfoByCourseId/${courseId}`,
+      url: `/education/edu-chapter/getChapterInfoByCourseId/${courseId}`,
       method: 'get'
     })
   }
--- vue-admin/src/views/education/course/Chapter.vue
+++ vue-admin/src/views/education/course/Chapter.vue
@@ -6,13 +6,28 @@
       <el-step title="创建课程大纲"/>
       <el-step title="提交审核"/>
     </el-steps>
-    <el-form label-width="120px" class="save_item">
-      <el-form-item>
-        <el-button @click="previous">上一步</el-button>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="next">下一步
-        </el-button>
-      </el-form-item>
-    </el-form>
+    <div class="chapter_container">
+      <el-form label-width="120px">
+        <el-form-item>
+          <ul class="chapterList">
+            <li v-for="chapter in chapterInfoList" :key="chapter.id">
+              <p>{{chapter.title}}</p>
+              <ul class="videoList">
+                <li v-for="video  in chapter.children" :key="video.id">
+                  <p>{{video.title}}</p>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </el-form-item>
+
+        <el-form-item class="save_item">
+          <el-button @click="previous">上一步</el-button>
+          <el-button :disabled="saveBtnDisabled" type="primary" @click="next">下一步
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 <script>
@@ -36,6 +51,7 @@
       getChapterInfoByCourseId() {
         chapter.getChapterInfoByCourseId(this.courseId)
           .then(response => {
+            console.log(response.data.chapterInfoList)
             this.chapterInfoList = response.data.chapterInfoList;
           })
       },
@@ -50,8 +66,59 @@
 </script>
 
 <style scoped>
+  .chapter_container {
+    /*居中样式*/
+    margin: 0 auto;
+    width: 70%;
+    margin-left: 10%;
+    margin-top: 3%;
+  }
+
   .save_item {
     margin-left: 38%;
     margin-top: 5%;
+  }
+
+  .chapterList {
+    position: relative;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .chapterList li {
+    position: relative;
+  }
+
+  .chapterList p {
+    float: left;
+    font-size: 20px;
+    margin: 10px 0;
+    padding: 10px;
+    height: 70px;
+    line-height: 50px;
+    width: 100%;
+    border: 1px solid #DDD;
+  }
+
+  .chapterList .acts {
+    float: right;
+    font-size: 14px;
+  }
+
+  .videoList {
+    padding-left: 50px;
+    list-style-type: none;
+  }
+
+  .videoList p {
+    float: left;
+    font-size: 14px;
+    margin: 10px 0;
+    padding: 10px;
+    height: 50px;
+    line-height: 30px;
+    width: 100%;
+    border: 1px dotted #DDD;
   }
 </style>
